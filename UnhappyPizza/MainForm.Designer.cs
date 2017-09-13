61a1cfa5645c584e67d1b70a6e94c8caf5397d8e
Veninger <mordana666@gmail.com>
2017-09-13 15:34:43 +0200

    Initial commit

---
+++ UnhappyPizza/MainForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace UnhappyPizza
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tbMain = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(249, 10);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "meflu";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tbMain
+            // 
+            this.tbMain.Location = new System.Drawing.Point(12, 42);
+            this.tbMain.Multiline = true;
+            this.tbMain.Name = "tbMain";
+            this.tbMain.Size = new System.Drawing.Size(231, 462);
+            this.tbMain.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 6);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(231, 30);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "http://fasz/";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 510);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbMain);
+            this.Controls.Add(this.btnStart);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox tbMain;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ UnhappyPizza/MainForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnhappyPizza
+{
+    public partial class MainForm : Form
+    {
+        private const int lenght = 6;
+        private string url;
+        private static char[] chars =
+        {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'a', 'b'
+        };
+
+        public MainForm()
+        {
+            InitializeComponent();
+            url = textBox1.Text;
+        }
+
+        public void StartBruteForce(int length)
+        {
+            StringBuilder sb = new StringBuilder(length);
+            char currentChar = chars[0];
+
+            for (int i = 1; i <= length; i++)
+            {
+                sb.Append(currentChar);
+            }
+
+            ChangeCharacters(0, sb, length);
+        }
+
+        private StringBuilder ChangeCharacters(int pos, StringBuilder sb, int length)
+        {
+            for (int i = 0; i <= chars.Length - 1; i++)
+            {
+                sb.Replace(sb[pos], chars[i], pos, 1);
+                if (pos == length - 1)
+                {
+                    tbMain.Invoke(new Action(() => tbMain.Text += url + sb.ToString() + Environment.NewLine));
+                }
+                else
+                {
+                    ChangeCharacters(pos + 1, sb, length);
+                }
+            }
+            return sb;
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Thread MainThread = new Thread(() => StartBruteForce(lenght));
+            MainThread.Start();
+        }
+    }
+}
